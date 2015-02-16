4500a6a860dbc82339c5c6324caca8981a2a047b
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2015-02-16 21:32:07 +0100

    Underscode

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var Person = require('../models/person').Person;
+var _ = require('underscore');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    var shufflePeople = function (callback) {
+        Person.find({}, function (err, people) {
+            var groups = [];
+            //console.log(_.sample(people, 4));
+
+            res.render('index', {people: people});
+        });
+    };
+
+    shufflePeople();
 });
 
-router.get('/import', function(req, res, next) {
+router.get('/import', function (req, res, next) {
     var createPerson = function (index) {
         var person = new Person;
         person.email = 'random.person+' + index + '@gmail.com';
